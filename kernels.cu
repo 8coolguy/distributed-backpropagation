60241a785b19cddbf2e2be6c67be76daf147f62b
mvcast77 <castillo2020var@gmail.com>
2024-11-26 12:59:05 -0800

    Bare bones work on the cuda functions, including an error check macro

---
+++ kernels.cu
@@ -0,0 +1,20 @@
+#include <cuda.h>
+#include <iostream>
+#include <assert.h>
+
+inline void gpuAssert(cudaError_t code, const char *file, int line, bool abort = true)
+{
+    if (code != cudaSuccess)
+    {
+	std::cout << "GPUassert: " << cudaGetErrorString(code) << \
+		", " << file << ", " << line << std::endl;
+        if (abort)
+            exit(code);
+    }
+}
+#define gpuErrorCheck(ans) {gpuAssert((ans), __FILE__, __LINE__);}
+
+__global__ void foo(void)
+{
+	std::cout << "skeleton example" << std::endl;
+}
